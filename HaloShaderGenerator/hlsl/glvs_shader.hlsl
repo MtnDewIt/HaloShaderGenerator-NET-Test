6ce40646393471cddd2338175ebe043dbf4ded39
Beatthezombie <26466801+Beatthezombie@users.noreply.github.com>
2020-04-06 05:12:27 -0700

    finish albedo rigid glvs

--- HaloShaderGenerator/hlsl/glvs_shader.hlsl
+++ HaloShaderGenerator/hlsl/glvs_shader.hlsl
@@ -1,28 +1,23 @@
-﻿//#include "registers\vertex_shader.hlsli"
-#include "registers\vertex_shader.hlsli"
+﻿#include "registers\vertex_shader.hlsli"
 #include "helpers\input_output.hlsli"
-#include "helpers\projection_math.hlsli"
+#include "helpers\transform_math.hlsli"
 
-// temporary definition, will have to macro the hell out of this
 VS_OUTPUT_ALBEDO entry_albedo_rigid(VS_INPUT_RIGID_VERTEX_ALBEDO input)
 {
     VS_OUTPUT_ALBEDO output;
 
-	output.texcoord.xy = input.texcoord.xy * uv_compression_scale_offset.xy + uv_compression_scale_offset.zw;
-	output.normal.xyz = transform_value(input.normal.xyz, nodes[0].xyz, nodes[1].xyz, nodes[2].xyz);
-	float3 temp_binormal = transform_binormal(input.normal.xyz, input.tangent.xyz, input.binormal.xyz);
-	output.binormal.xyz = transform_value(temp_binormal, nodes[0].xyz, nodes[1].xyz, nodes[2].xyz);
-	output.tangent.xyz = transform_value(input.tangent.xyz, nodes[0].xyz, nodes[1].xyz, nodes[2].xyz);
-	float4 vertex_position = input.position * position_compression_scale + position_compression_offset; //model space
-	vertex_position.w = 1.0;
-	vertex_position.x = dot(vertex_position, nodes[0]);
-	vertex_position.y = dot(vertex_position, nodes[1]);
-	vertex_position.z = dot(vertex_position, nodes[2]);
+	float3x3 node_transformation = float3x3(nodes[0].xyz, nodes[1].xyz, nodes[2].xyz);
+	float4x4 v_node_transformation = float4x4(nodes[0], nodes[1], nodes[2], float4(0,0,0,0));
+	
+	output.texcoord.xy = calculate_texcoord(input.texcoord);
+	output.normal.xyz = transform_vector(input.normal.xyz, node_transformation);
+	output.binormal.xyz = transform_vector(transform_binormal(input.normal.xyz, input.tangent.xyz, input.binormal.xyz), node_transformation);
+	output.tangent.xyz = transform_vector(input.tangent.xyz, node_transformation);
+	float4 vertex_position = float4(decompress_vertex_position(input.position.xyz), 1.0);
+	vertex_position.xyz = mul(v_node_transformation, vertex_position.xyzw).xyz;
+	float4 screen_position = calculate_screenspace_position(vertex_position);
 	output.camera_dir = camera_position - vertex_position.xyz;
-	vertex_position = mul(vertex_position, view_projection);
-	vertex_position.z = v_squish_params.w * (v_squish_params.z * (v_squish_params.x * vertex_position.w - v_squish_params.y) - vertex_position.z) + vertex_position.z;
-	output.position = vertex_position.xyzw;
-	output.normal.w = vertex_position.w;
+	output.position = screen_position.xyzw;
+	output.normal.w = screen_position.w;	
 	return output;
 }
-// TODO: verify compiled output for v_squish_params
---
+++ HaloShaderGenerator/hlsl/helpers/transform_math.hlsli
@@ -0,0 +1,41 @@
+﻿#ifndef _TRANSFORM_MATH_HLSLI
+#define _TRANSFORM_MATH_HLSLI
+
+#include "../registers/vertex_shader.hlsli"
+
+float3 transform_vector(float3 dir, float3x3 transform)
+{
+	return normalize(mul(transform, dir));
+}
+
+float3 transform_binormal(float3 normal, float3 tangent, float3 binormal)
+{
+	float3 computed_binormal = cross(normal, tangent);
+	float bin_sign = sign(dot(computed_binormal, binormal));
+	return bin_sign * binormal;
+}
+
+float3 decompress_vertex_position(float3 position)
+{
+	return (position * position_compression_scale.xyz) + position_compression_offset.xyz;
+}
+
+float calculate_z_squish(float4 screen_position)
+{
+	return v_squish_params.w * ((v_squish_params.x * screen_position.w - v_squish_params.y) * v_squish_params.z - screen_position.z) + screen_position.z;
+}
+
+float4 calculate_screenspace_position(float4 vertex_position)
+{
+	float4 screen_position = mul(vertex_position, view_projection);
+	screen_position.z = calculate_z_squish(screen_position);
+	return screen_position;
+}
+
+float2 calculate_texcoord(float4 texcoord)
+{
+	return texcoord.xy * uv_compression_scale_offset.xy + uv_compression_scale_offset.zw;
+}
+
+
+#endif
