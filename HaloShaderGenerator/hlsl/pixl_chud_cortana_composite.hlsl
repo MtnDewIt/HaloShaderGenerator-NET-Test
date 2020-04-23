5dc214a4adcf299007ea81b160c2eb3bfdeb5735
Beatthezombie <26466801+Beatthezombie@users.noreply.github.com>
2020-04-23 05:39:46 -0700

    more fixes for cortana_composite

--- HaloShaderGenerator/hlsl/pixl_chud_cortana_composite.hlsl
+++ HaloShaderGenerator/hlsl/pixl_chud_cortana_composite.hlsl
@@ -8,32 +8,38 @@ float4 main(VS_OUTPUT_CORTANA_COMPOSITE input) : COLOR
 	
 	
 	float distance_to_frag = length(input.v1 - 0.5);
+	float2 texcoord = input.v0 * input.v3;
 	
-	float vignette = (distance_to_frag - cortana_vignette_data.x) / (cortana_vignette_data.y - cortana_vignette_data.x);
-	vignette = saturate(vignette);
-	vignette *= vignette;
+	float4 basemap_sample = tex2D(basemap_sampler, texcoord);
+	float4 cortana_sample = tex2D(cortana_sampler, texcoord);
+	float4 goo_sample = tex2D(goo_sampler, input.v2.xy);
 	
-	
-	
-	float solarize = 1.0 / (1.0 - cortana_comp_solarize_result.x);
-	
-	float4 combined_texcoord;
-	combined_texcoord.xy = input.v0;
-	combined_texcoord = combined_texcoord.xyxy * input.v3;
+	float vignette = saturate((distance_to_frag - cortana_vignette_data.x) / (cortana_vignette_data.y - cortana_vignette_data.x));
+	float2 unknown_vignette = (cortana_sample.x * 0.5 + vignette * vignette) * float2(0.4, 0.6);
+	float4 color_mix_scale = 1.0 - (unknown_vignette.x * goo_sample + unknown_vignette.y);
 	
 	
 	
-	float4 texcoord = 2.0 * combined_texcoord - 1.0;
-	float2 scales = float2(0.8, 0.5) * cortana_comp_doubling_result.z;
-	texcoord = (scales.xxyy * texcoord) * 0.5 + 0.5; // invert 2 * - 1
 	
-	float4 sample1 = tex2D(cortana_sampler, texcoord.xy);
-	float4 sample2 = tex2D(cortana_sampler, texcoord.zw);
+	
+	
+	
+	
+	float2 transformed_texcoord = 2.0 * texcoord - 1.0;
+	float scale_1 = 0.8 * cortana_comp_doubling_result.z;
+	float scale_2 = 0.5 * cortana_comp_doubling_result.z;
+	float2 texcoord_scale_1 = transformed_texcoord * scale_1;
+	float2 texcoord_scale_2 = transformed_texcoord * scale_2;
+	texcoord_scale_1 = 0.5 + 0.5 * texcoord_scale_1;
+	texcoord_scale_2 = 0.5 + 0.5 * texcoord_scale_2;
+
+	
+	float4 sample1 = tex2D(cortana_sampler, texcoord_scale_1);
+	float4 sample2 = tex2D(cortana_sampler, texcoord_scale_2);
 	float4 sampler_result = sample1 + sample2;
-	
 	float doubling_in_mix = dot(sampler_result, cortana_comp_doubling_inmix);
-	sampler_result.a = 1.0;
-	float4 basemap_sample = tex2D(basemap_sampler, combined_texcoord.zw);
+
+	
 	
 	color_mix.a = 1.0;
 	color_mix.rgb = basemap_sample.rgb * cortana_back_colormix_result.xyz;
@@ -42,21 +48,21 @@ float4 main(VS_OUTPUT_CORTANA_COMPOSITE input) : COLOR
 	
 	
 	float4 outmix = cortana_comp_doubling_result.x * cortana_comp_doubling_outmix;
-	float4 cortana_sample = tex2D(cortana_sampler, combined_texcoord.zw);
-	float2 unknown_vignette = (cortana_sample.x * 0.5 + vignette) * float2(0.4, 0.6);
+	
 	
 	outmix = outmix * doubling_in_mix + cortana_sample;
 	
-	
+	float solarize = 1.0 / (1.0 - cortana_comp_solarize_result.x);
 	float solarize_mix = min(dot(outmix, cortana_comp_solarize_inmix), 1.0);
 	solarize_mix = solarize_mix - cortana_comp_solarize_result.x;
 	solarize = solarize * solarize_mix;
 	
 	float exp_solarize = pow(solarize, cortana_comp_solarize_result.y);
 
-	float solarize_scale = solarize_mix < 0 ? exp_solarize : 0;
+	float solarize_scale = solarize_mix < 0 ? 1 : 0;
+	solarize_scale *= exp_solarize;
 	
-	float4 color_result = solarize_scale * cortana_comp_solarize_outmix + sampler_result;
+	float4 color_result = solarize_scale * cortana_comp_solarize_outmix + outmix;
 	
 	[unroll]
 	if (chud_comp_colorize_enabled)
@@ -67,8 +73,8 @@ float4 main(VS_OUTPUT_CORTANA_COMPOSITE input) : COLOR
 
 	color_result.rgb = srgb_to_rgb(color_result.rgb);
 	
-	float4 goo_sample = tex2D(goo_sampler, input.v2.xy);
-	float4 color_mix_scale = 1.0 - (unknown_vignette.x * goo_sample + unknown_vignette.y);
+	
+	
 	
 	
 	return color_mix * color_mix_scale + color_result;
