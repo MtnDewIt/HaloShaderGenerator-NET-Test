f26b401a2e81c46dcedbfd7325a16e6dd4ff356e
Beatthezombie <26466801+Beatthezombie@users.noreply.github.com>
2020-08-07 06:34:32 -0400

    fix rmbk yet again

--- HaloShaderGenerator/hlsl/glvs_shader_black.hlsl
+++ HaloShaderGenerator/hlsl/glvs_shader_black.hlsl
@@ -14,9 +14,11 @@ VS_OUTPUT_BLACK_ALBEDO entry_albedo(input_vertex_format input)
 	float3 binormal;
 	float2 texcoord;
 	float3 sky_radiance;
+	float3 extinction_factor;
 	calc_vertex_transform(input, world_position, output.position, normal, tangent, binormal, texcoord);
     float3 camera_dir = Camera_Position - world_position.xyz;
 	calculate_z_squish(output.position);
-	calculate_atmosphere_radiance(world_position, camera_dir, output.color.rgb, sky_radiance);
+	calculate_atmosphere_radiance(world_position, camera_dir, extinction_factor, sky_radiance);
+	output.color = sky_radiance;
 	return output;
 }
