e7f27e2ac73ad0d1601132be415971b3ed4560fc
Pedro413 <44695680+Pedro413@users.noreply.github.com>
2023-03-02 19:46:53 +1100

    console queue

---
+++ HaloShaderGeneratorTest/QueuedConsole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaloShaderGenerator
+{
+    static class QueuedConsole
+    {
+        private static object Lock = new object();
+
+        public static void QueueMessage(string message, ConsoleColor colour)
+        {
+            lock (Lock)
+            {
+                Console.ForegroundColor = colour;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
