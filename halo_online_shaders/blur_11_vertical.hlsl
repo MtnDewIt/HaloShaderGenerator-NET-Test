a2fc0231f61d078db4861f1fed90530af8ed14f4
Pedro413 <44695680+Pedro413@users.noreply.github.com>
2025-10-21 10:15:33 +1100

    update bloom and blur

--- halo_online_shaders/blur_11_vertical.hlsl
+++ halo_online_shaders/blur_11_vertical.hlsl
@@ -1,8 +1,5 @@
 #line 2 "source\rasterizer\hlsl\blur_11_vertical.hlsl"
 
-#define XENON_PIXEL_SIZE float2(1/1152.0f,1/648.0f)
-#define BLUR_KERNEL_RADIUS 5
-
 #include "global.fx"
 #include "hlsl_vertex_types.fx"
 #include "utilities.fx"
@@ -10,51 +7,10 @@
 //@generate screen
 
 LOCAL_SAMPLER_2D(target_sampler, 0);
-//float4 kernel[11] : register(c2);		// c2 through c12 are the kernel (r,g,b)
-
-float2 calculate_pixel_scale(float2 size)
-{
-    return XENON_PIXEL_SIZE / size;
-}
-
-float get_gaussian_distribution_weight(float sigma, int weight_index)
-{
-    float x = weight_index / sigma;
-    float weight = exp(-x * x * 4.5);
-    return weight;
-}
-
-float4 dynamic_blur_vertical(float2 texcoord)
-{	
-    float4 sample_occlusion = 0.0f;
-    float4 weighted_occlusion_sum = 0.0f;
-    float total_weight = 0.0f;
-
-    float filter_radius = max(BLUR_KERNEL_RADIUS * calculate_pixel_scale(pixel_size.xy), 2);
-
-    int number_of_taps = filter_radius * 2 + 1;
-    float4 sample_texcoord = 0.0f;
-
-	[loop]
-    for (int sample_index = 0; sample_index < number_of_taps; sample_index++)
-    {
-        float location_delta = sample_index - filter_radius; // Distance from center of filter
-        sample_texcoord.xy = float2(texcoord.x, texcoord.y + location_delta * pixel_size.y);
-        sample_occlusion = tex2Dlod(target_sampler, float4(sample_texcoord.xy, 0, 0)).rgba;
-		
-        float fSampleWeight = get_gaussian_distribution_weight(filter_radius, location_delta);
-        weighted_occlusion_sum += fSampleWeight * sample_occlusion;
-        total_weight += fSampleWeight;
-    }
-
-    return weighted_occlusion_sum / total_weight;
-}
 
 fast4 default_ps(screen_output IN) : SV_Target
 {
 	float2 sample= IN.texcoord;
-	
-    return dynamic_blur_vertical(sample);
 	
 	// solution using bilinear filtering:
 	// actually this is a 10 wide blur - you get the 11th pixel by offsetting the vertical blur by half a pixel
@@ -110,13 +66,11 @@ fast4 default_ps(screen_output IN) : SV_Target
 			{0.5,	+4.0 - 9.0/(1.0+9.0)			}			// +3.1
 		};
 	
-    float2 pixel_size_scale = float2(1.0f / 1152, 1.0f / 640) / pixel_size.xy;
-	
-	float4 color=	(1.0   + 9.0)	* sample2D(target_sampler, sample + offset[0] * pixel_size * pixel_size_scale) +
-					(36.0  + 84.0)	* sample2D(target_sampler, sample + offset[1] * pixel_size * pixel_size_scale) +
-					(126.0 + 126.0)	* sample2D(target_sampler, sample + offset[2] * pixel_size * pixel_size_scale) +
-					(84.0  + 36.0)	* sample2D(target_sampler, sample + offset[3] * pixel_size * pixel_size_scale) +
-					(1.0   + 9.0)	* sample2D(target_sampler, sample + offset[4] * pixel_size * pixel_size_scale);
+	float4 color=	(1.0   + 9.0)	* sample2D(target_sampler, sample + offset[0] * pixel_size) +
+					(36.0  + 84.0)	* sample2D(target_sampler, sample + offset[1] * pixel_size) +
+					(126.0 + 126.0)	* sample2D(target_sampler, sample + offset[2] * pixel_size) +
+					(84.0  + 36.0)	* sample2D(target_sampler, sample + offset[3] * pixel_size) +
+					(1.0   + 9.0)	* sample2D(target_sampler, sample + offset[4] * pixel_size);
 
 	return color / 512.0;
 }
--- halo_online_shaders/kernel_5.hlsl
+++ halo_online_shaders/kernel_5.hlsl
@@ -1,8 +1,5 @@
 #line 2 "source\rasterizer\hlsl\kernel_5.hlsl"
 
-#define XENON_PIXEL_SIZE float2(1/1152.0f,1/648.0f)
-#define BLUR_KERNEL_RADIUS 5
-
 #include "global.fx"
 #include "hlsl_vertex_types.fx"
 #include "utilities.fx"
@@ -12,58 +9,15 @@
 
 LOCAL_SAMPLER_2D(target_sampler, 0);
 
-float2 calculate_pixel_scale(float2 size)
-{
-    return XENON_PIXEL_SIZE / size;
-}
-
-float get_gaussian_distribution_weight(float sigma, int weight_index)
-{
-    float x = weight_index / sigma;
-    float weight = exp(-x * x * 4.5);
-    return weight;
-}
-
-float4 dynamic_blur_vertical(float2 texcoord)
-{	
-    float4 sample_occlusion = 0.0f;
-    float4 weighted_occlusion_sum = 0.0f;
-    float total_weight = 0.0f;
-
-    float filter_radius = max(BLUR_KERNEL_RADIUS * calculate_pixel_scale(pixel_size.xy), 2);
-
-    int number_of_taps = filter_radius * 2 + 1;
-    float4 sample_texcoord = 0.0f;
-
-	[loop]
-    for (int sample_index = 0; sample_index < number_of_taps; sample_index++)
-    {
-        float location_delta = sample_index - filter_radius; // Distance from center of filter
-        sample_texcoord.xy = float2(texcoord.x, texcoord.y + location_delta * pixel_size.y);
-        sample_occlusion = tex2Dlod(target_sampler, float4(sample_texcoord.xy, 0, 0)).rgba;
-		
-        float fSampleWeight = get_gaussian_distribution_weight(filter_radius, location_delta);
-        weighted_occlusion_sum += fSampleWeight * sample_occlusion;
-        total_weight += fSampleWeight;
-    }
-
-    return weighted_occlusion_sum / total_weight;
-}
-
-float4 default_ps(screen_output IN) : SV_Target
+fast4 default_ps(screen_output IN) : SV_Target
 {
 	float2 sample= IN.texcoord;
-    
-    // kernel_5 supplied is always identical to hardcoded kernel in blur_11_vertical
-    return dynamic_blur_vertical(sample) * scale;
 	
-    float2 pixel_size_scale = float2(1.0f / 1152, 1.0f / 640) / pixel_size.xy;
-	
-	float4 color=	kernel[0].z * sample2D(target_sampler, sample + kernel[0].xy * pixel_size.xy * pixel_size_scale) +
-					kernel[1].z * sample2D(target_sampler, sample + kernel[1].xy * pixel_size.xy * pixel_size_scale) +
-					kernel[2].z * sample2D(target_sampler, sample + kernel[2].xy * pixel_size.xy * pixel_size_scale) +
-					kernel[3].z * sample2D(target_sampler, sample + kernel[3].xy * pixel_size.xy * pixel_size_scale) +
-					kernel[4].z * sample2D(target_sampler, sample + kernel[4].xy * pixel_size.xy * pixel_size_scale);
+	float4 color=	kernel[0].z * sample2D(target_sampler, sample + kernel[0].xy) +
+					kernel[1].z * sample2D(target_sampler, sample + kernel[1].xy) +
+					kernel[2].z * sample2D(target_sampler, sample + kernel[2].xy) +
+					kernel[3].z * sample2D(target_sampler, sample + kernel[3].xy) +
+					kernel[4].z * sample2D(target_sampler, sample + kernel[4].xy);
 
 	return color * scale;
 }
